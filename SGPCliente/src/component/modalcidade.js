5aa8789d343cb3dc87edd2905b53f76f4963dd8c
Jean Max Guezzo <jean.max.guezzo@gmail.com>
2019-10-30 00:40:30 -0300

    Update modalcidade.js
    update Select

--- SGPCliente/src/component/modalcidade.js
+++ SGPCliente/src/component/modalcidade.js
@@ -8,12 +8,14 @@ import { Modal,
     Form, Input, Col, Label} from 'reactstrap';
     import api from './api';
     import grupoCampos from './campos'
+import { async } from 'q';
 
     export default function ModalCidade ({history, match}){  
     const [modal, setModal] = useState(true);
     const [id, setId] = useState(''); 
     const [nome, setNome] = useState('');
-    const [estadoNome, setEstadoNome] = useState('');
+    const [estadoid, setEstadoId] = useState('');
+    const [estadonome, setEstadoNome] = useState('');
     const [estado, setEstado] = useState([]);  
     
      const toggle = () => setModal(!modal, history.push('/listagem/cidade/') );
@@ -24,19 +26,26 @@ import { Modal,
       const response = await api.get('/api/cidade/'+ Parametro);
       setId(response.data.id);
       setNome(response.data.nome);
-      setEstadoNome(response.data.cidadeestado);
+      setEstadoId(response.data.estadoId);
       }
       await api.get('/api/estado/')
       .then(response => {
         const { data } = response;
           setEstado( data)
-      })
-      .catch(err => {
-        console.warn(err);
-        alert(err.message)
-      })
+      })     
       
       }; 
+
+      async function PesquisaEstado (){
+        await api.get('/api/estado/'+ estadoid)
+      .then(response => {
+        const { data } = response;
+          setEstadoNome(data.nome) 
+          
+      })
+      }
+      PesquisaEstado();
+     
     async function componentAll () {
       if(match.params.Id != null){
         const responseAlter = await api.put('/api/cidade/'+ id,{
@@ -51,6 +60,7 @@ import { Modal,
     const responseAdd =  await api.post('/api/cidade/'+ id,{
       id,
       nome,
+
   })
   if(responseAdd.status === 201){
     alert("Cidade Cadastrada com Sucesso.");
@@ -59,7 +69,7 @@ import { Modal,
 }
 }
 const handleChange = (event) => {
- const Event =  event => setEstadoNome(event.target.value);
+ const Event =  event => setEstadoId(event.target.value);
  return Event;
 }
     return(
@@ -73,6 +83,7 @@ const handleChange = (event) => {
                 <Col xs="8">
                 <Label>Estado</Label>
                 <Input type="select" name="select" onChange={handleChange()}>
+                  <option selected value={estadoid}>{estadonome}</option>
                   {estado.map(list =>(
                     <option value={list.id}>
                       {list.nome}
